08847e51d16251c03fec4fb50a6a4c38b95b885d
Kezeneilhou <mkezneneilhou@gmail.com>
2022-03-06 20:27:55 +0530

    ss

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,10 @@
 		<!-- Basic -->
 		<meta charset="UTF-8">
 
-		<title>Default Layout | Porto Admin - Responsive HTML5 Template</title>
+		<title>TRAC: Nagaland</title>
 		<meta name="keywords" content="HTML5 Admin Template" />
-		<meta name="description" content="Porto Admin - Responsive HTML5 Template">
-		<meta name="author" content="okler.net">
+		<meta name="description" content="TRAC Software">
+		<meta name="author" content="Kezeneilhour">
 
 		<!-- Mobile Metas -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
